daf8eeb43d9be0f96a0b47c403fc6e9479f205e3
shahbaz1957 <shahbazjamia1957@gmail.com>
2024-08-07 20:25:06 +0530

    ContextAPI and LocalStorage

--- 09themeSwitcher/src/contexts/theme.js
+++ 09themeSwitcher/src/contexts/theme.js
@@ -13,3 +13,6 @@ export const ThemeProvider = ThemeContext.Provider
 
 // Use useTheme hook in your components
 export const useTheme = () => useContext(ThemeContext)
+
+// This link help to understand how Context API works
+// https://www.freecodecamp.org/news/react-context-api-explained-with-examples/
